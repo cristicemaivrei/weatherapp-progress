1149ba632176d2dae4eda402aacfccc4a60934d3
Cristi T <89873995+cristicemaivrei@users.noreply.github.com>
2024-03-20 20:11:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "a17ebec192b55d92a9c0ddb947f5b916";
+const apiKey = ""; // use your own api key from openweathermap.org
 
 weatherForm.addEventListener("submit", async event => {
     event.preventDefault();
-    const city = cityInput.value; // Get the value of the input field here
+    const city = cityInput.value;
 
     if(city) {
         try {
-            const weatherData = await getWeatherData(city); // Pass city as an argument
+            const weatherData = await getWeatherData(city);
             displayWeatherInfo(weatherData);
         }
         catch(error) {
@@ -23,14 +23,14 @@ weatherForm.addEventListener("submit", async event => {
 async function getWeatherData(city) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     const response = await fetch(apiUrl);
-    const weatherData = await response.json(); // Parse response as JSON
+    const weatherData = await response.json();
     console.log(weatherData);
 }
 
 function displayWeatherInfo(data) {
-    // Implement this function to display weather information on the UI
+    
 }
 
 function getWeatherEmoji(weatherId) {
-    // Implement this function to get weather emoji based on weather ID
+    
 }
